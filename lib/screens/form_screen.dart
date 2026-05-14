cc36ec631f618fdd9eba5beea2f0d05900b44697
Mongkhon-w <sekmongkol123@gmail.com>
2026-05-14 16:21:43 +0700

    feat_add_form_button

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -6,8 +6,38 @@ class FormScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("แบบฟอร์มบันทึกข้อมูล")),
-      body: Container(),
+      appBar: AppBar(title: const Text("แบบฟอร์มบันทึกข้อมูล")),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: "ชื่อรายการ"),
+                autofocus: true,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: "จำนวนเงิน"),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(
+                height: 20,
+              ), // เพิ่มช่องว่างระหว่าง Text field กับปุ่ม
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple, // สีพื้นหลังปุ่ม
+                  foregroundColor: Colors.white, // สีตัวอักษร
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("เพิ่มข้อมูล"),
+              ), // ElevatedButton
+            ], // Children ของ Column
+          ),
+        ),
+      ),
     );
   }
 }
